f72c496a346c5d0f76e37743156daa23f81440f5
kobori <ubiratan.jp@outlook.com>
2019-05-23 15:47:05 +0900

    Commit cp-04-1

--- app/controllers/locations.js
+++ app/controllers/locations.js
@@ -5,7 +5,7 @@ module.exports.homelist = (req, res,) => {
 
 /* GET 'Location info' page */
 module.exports.locationInfo = (req, res) => {
-  res.render('index', {title: 'Location info'});
+  res.render('location-info', {title: 'Location info'});
 };
 
 /* GET 'Add review' page */
